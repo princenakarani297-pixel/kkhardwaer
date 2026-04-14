f33c100c853199d584b19fdc75bf9de5fbab8ff8
Prince nakrani <princenakarani297@gmail.com>
2026-04-14 21:58:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ app.post("/send", async (req, res) => {
 });
 
 // ================= START SERVER =================
-app.listen(3001, () => {
-    console.log("🚀 Server running at http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
